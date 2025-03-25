7a6918d60a47912c9b219ef9018440934378ab5c
thermaldeveloper <thermaldeveloper@icloud.com>
2025-03-25 06:29:46 -0500

    Initial commit

---
+++ HowHot/HowHotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowHotApp.swift
+//  HowHot
+//
+//  Created by Brad Rice on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HowHotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HowHotTests/HowHotTests.swift
@@ -0,0 +1,17 @@
+//
+//  HowHotTests.swift
+//  HowHotTests
+//
+//  Created by Brad Rice on 3/25/25.
+//
+
+import Testing
+@testable import HowHot
+
+struct HowHotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
